98e328c26c547e56fe4e28c288bb9f239fa36fca
admin <89615446444@mail.ru>
2021-06-01 12:10:50 +0300

    test

---
+++ class_work/lesson1.py
@@ -0,0 +1,9 @@
+print("Hello World")  # statement
+print("Welcome to Python")  # statement
+if 1 < 2:
+    print("number one low number  two")
+
+# print() Вывод на консоль
+# input() ввод на консоль
+name = input("Введите свое имя: ")
+print("Hello",name)
